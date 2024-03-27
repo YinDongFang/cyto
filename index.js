18e7b8c6f93d51490df3c2b464c8071a0dba8714
Yin <doyin@deloitte.com.cn>
2024-03-27 16:43:29 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const print = require('./print')
+const flattenArray = require('./flattenArray')
+const deepCopy = require('./deepCopy')
+const factorial = require('./factorial')
+const stringToNumberArray = require('./stringToNumberArray')
+
+module.exports = (num) => {
+    return num % 2 !== 0;
+  }
